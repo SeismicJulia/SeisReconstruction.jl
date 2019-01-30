936553e2abaeb9e74e62a3d2245a915e7375ceaf
fercarozzi <carozzi@ualberta.ca>
2019-01-30 11:06:11 -0700

    add travis file

---
+++ test/test_MWNI.jl
@@ -0,0 +1,14 @@
+using SeisProcessing
+using SeisReconstruction
+using LinearAlgebra
+using Base.Test
+
+d = SeisLinearEvents()
+ddec = SeisDecimate(d,perc=50)
+
+dmwni = SeisMWNI(ddec,dt=0.004,fmax=100,Niter_external=5,Niter_internal=10)
+
+# test that quality factor is greater than 10 Decibels
+quality_factor = 10*log10(norm(d[:],2)/norm(dmwni[:]-d[:],2))
+println("Quality factor = ",quality_factor)
+@test quality_factor > 5
