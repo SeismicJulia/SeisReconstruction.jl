304c0d77061ced65bebf990b94d2dbfb475b536e
Joaquin Acedo <aacedo@ualberta.ca>
2024-03-14 08:15:56 -0600

    Export new functions and add dependencies

--- src/SeisReconstruction.jl
+++ src/SeisReconstruction.jl
@@ -1,9 +1,17 @@
 module SeisReconstruction
-    using Interpolations,FFTW, LinearAlgebra,DSP
-    include("Reconstruction/Reconstruction.jl")
-    include("Tools/Tools.jl")
+    
+    #Dependencies
+    using Interpolations
+    using FFTW
+    using LinearAlgebra
+    using DSP
+    using Statistics
+    using Printf
 
 
+    #Export functions
+    export ADMM
+    export CGLS
     export CalculateSampling
     export ConjugateGradients
     export Convmtx
