267a8c277b3ff36a082c347b32f8d541e1a60168
joaquin-17 <aacedo@ualberta.ca>
2025-04-02 18:20:47 -0600

    FISTA example  is working

--- examples/FISTA_reconstruction_example.jl
+++ examples/FISTA_reconstruction_example.jl
@@ -3,6 +3,7 @@ using SeisReconstruction
 using SeisProcessing
 using LinearAlgebra
 using DSP
+using PyPlot
 
 dt=0.002; # dt 
 nt=512; #number of simples in time dimension
@@ -18,71 +19,74 @@ p1=[0.15,0.2, 0.15]; # ray paramteter dimension2
 
 
 
-dtrue=SeisParabEvents( tau=tau,amp=amp, p2=p2,p1=p1, dt=dt, nt=nt, dx1=dx1, nx1=nx1, dx2=dx2, nx2=nx2, f0=f0);
-
-nt,nr,ns=size(dtrue);
-
-dobs =(10^2)*copy(dtrue); # scale
-
-dobsn= SeisAddNoise(dobs, 1.0, db=false, L=6);
+dtrue=SeisParabEvents( tau=tau,amp=amp, p2=p2,p1=p1, dt=dt, nt=nt, dx1=dx1, nx1=nx1, dx2=dx2, nx2=nx2, f0=f0)*(10^2);
+dtruen=SeisAddNoise(dtrue, 1.0, db=false, L=6);
 
 
-dobsn=SeisDecimate(dobsn;mode="random",perc=50);
+nt,nr,ns=size(dtruen);
+
+dobsn =copy(dtruen); # scale
+
+#dobsn= SeisAddNoise(dobs, .8, db=false, L=6);
+
+
+dobsn=SeisDecimate(dobsn;mode="random",perc=40);
 S = CalculateSampling(dobsn)
 
-patch_size=(64,16,16); #Patch size in LocalFourier Operator5mu=μ,to
-Noverlap=(32,8,8); #Overlap of patches in LocalFourier Operator
-dims=size(dobs); #Parameter to recover the right dimensions.
-μ= 11.0;
-tolout=1.e-6;
+patch_size=(32,16,16); #Patch size in LocalFourier Operator5mu=μ,to
+Noverlap=(16,8,8); #Overlap of patches in LocalFourier Operator
+dims=size(dobsn); #Parameter to recover the right dimensions.
+μ= 5.0;
+tolout=1.e-10;
 parameters=  [Dict(:w => S),
 Dict(:patch_size=>patch_size, :Noverlap=>Noverlap, :dims=>dims, :normalize=>true, :padd=>false)];
 operators=[WeightingOp, LocalFFTOp];
-x0 = zeros(ComplexF64,spec_size(dobs,patch_size,Noverlap));
-m, J = FISTA(x0,dobsn,operators,parameters, μ=μ,Ni=50,tol=tolout);
+x0 = zeros(ComplexF64,spec_size(dobsn,patch_size,Noverlap));
+m, J = FISTA(x0,dobsn,operators,parameters, μ=μ,Ni=200,tol=tolout);
 
 
 
 drec=real(LocalFFTOp(m,false; patch_size, Noverlap, dims, normalize=true, padd=false));
 
 
+difference = drec .- dtrue;
 
 # Example slice index and data (replace with your actual data)
 ix = 32  # Inline or crossline index
 
 # Create figure
-fig = figure(fignum=1,figsize=(10, 10))
-
+fig = PyPlot.figure(fignum=1, figsize=(15, 12))
 # Panel 1: dtrue
-subplot(131)
-SeisPlotTX(dtrue[:, :, ix], style="wiggles", pclip=95, fignum=1)
+subplot(141)
+SeisPlotTX(dtrue[:, :, ix], style="wiggles", pclip=95, dx=25.0, dy=0.004, fignum=1, wiggle_trace_increment=3)
 title("dtrue")
-xlabel("Receiver")
-ylabel("Time")
+xlabel("Offset (m)")
+ylabel("Time (s)")
 gca().tick_params(labelsize=10)
 
 # Panel 2: dobs
-subplot(132)
-SeisPlotTX(dobsn[:, :, ix], style="wiggles", pclip=95, fignum=1)
+subplot(142)
+SeisPlotTX(dobsn[:, :, ix],style="wiggles", pclip=95, dx=25.0, dy=0.004, fignum=1, wiggle_trace_increment=2)
 title("dobs")
-xlabel("Receiver")
-ylabel("Time")
+xlabel("Offset (m)")
+ylabel("Time (s)")
 gca().tick_params(labelsize=10)
 
 # Panel 3: drec
-subplot(133)
-SeisPlotTX(drec[:, :, ix], style="wiggles", pclip=95, fignum=1)
+subplot(143)
+SeisPlotTX(drec[:, :, ix], style="wiggles", pclip=95, dx=25.0, dy=0.004, fignum=1, wiggle_trace_increment=2)
 title("drec")
-xlabel("Receiver")
-ylabel("Time")
+xlabel("Offset (m)")
+ylabel("Time (s)")
 gca().tick_params(labelsize=10)
 
 # Panel 4: difference
-#subplot(224)
-#SeisPlotTX(drec[:, :, ix] - dtrue[:, :, ix], cmap="seismic", interpolation="bilinear", pclip=95)
-#title("difference")
-#xlabel("Receiver")
-#ylabel("Time")
-#gca().tick_params(labelsize=10)
+subplot(144)
+SeisPlotTX(difference[:,:,ix], style="wiggles", pclip=95, dx=25.0, dy=0.004, fignum=1, wiggle_trace_increment=2)
+title("drec")
+title("difference")
+xlabel("Offset (m)")
+ylabel("Time (s)")
+gca().tick_params(labelsize=10)
 
 tight_layout()
